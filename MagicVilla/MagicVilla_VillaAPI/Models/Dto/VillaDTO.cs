9f2a8174268e45fc60653300f0e281b252d95557
andyvuong <vuongtrian116@gmail.com>
2024-06-13 10:58:41 -0500

    complete section 3 - CRUD functionality

---
+++ MagicVilla/MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagicVilla_VillaAPI.Models.Dto
+{
+    public class VillaDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        public int Occupancy { get; set; }
+        public int Sqft { get; set; }
+    }
+}
